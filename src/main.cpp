ae8e5b1ab0d774d55ac8528220ad50b628a3257f
huzhiping-hertz <huzhiping001@126.com>
2019-01-28 17:41:21 +0800

    cpp template

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <SquareRoot.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello Easy C++ project!" << std::endl;
+	string str = "aa";
+	cout << str << endl;
+	cout << SquareRoot(4)<< endl; 
+	return 0;
+}
---
+++ src/utility/include/SquareRoot.h
@@ -0,0 +1,1 @@
+double SquareRoot(const double a);
